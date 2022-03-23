271cb9062a26394361349a70d19198f2f0172328
Wyse- <5454041+Wyse-@users.noreply.github.com>
2022-03-23 21:18:51 +0100

    Improved encode script: h264 file is now flagged as vfr

--- Koi Kaze/encode_script/encode.sh
+++ Koi Kaze/encode_script/encode.sh
@@ -1,8 +1,12 @@
 cd ..
 mkdir encode
-for i in *.vpy ; do
-	vspipe -c y4m "$i" --timecodes encode/"$i"_timecodes.dat - | x264 --demuxer y4m - --output encode/"$i".x264 --preset placebo --deblock -2:-2 --crf 13 --merange 32 --no-dct-decimate --no-mbtree --qcomp 0.75 --aq-mode 3 --aq-strength 0.7 --output-depth 10
-	cd encode/	
-	mkvmerge -o "$i".mkv --timecodes 0:"$i"_timecodes.dat "$i".x264
+for i in `ls *.vpy -1v` ; do
+	vspipe -c y4m "$i" --timecodes encode/"$i"_timecodes.dat encode/"$i".y4m
+	x264 --demuxer y4m encode/"$i".y4m --output encode/"$i".h264 --tcfile-in encode/"$i"_timecodes.dat --preset placebo --deblock -2:-2 --crf 13 --merange 32 --no-dct-decimate --no-mbtree --qcomp 0.75 --aq-mode 3 --aq-strength 0.7 --output-depth 10
+	cd encode/
+	mkvmerge -o "$i".mkv --timecodes 0:"$i"_timecodes.dat "$i".h264
+	rm "$i".y4m
+	rm "$i"_timecodes.dat
+	rm "$i".h264
 	cd ..
 done
